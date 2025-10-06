decf61abeb8c4d457f33eb31e1266caa35b89ec2
javier <Javier.Yago@alu.uclm.es>
2025-10-06 18:54:40 +0200

    commit con los primeros dos pasos dle sprint1

---
+++ cliente/modelo.js
@@ -0,0 +1,27 @@
+function Sistema(){
+ this.usuarios={};
+ this.agregarUsuario=function(nick){
+ this.usuarios[nick]=new Usuario(nick);
+ }
+ this.obtenerUsuarios=function(){
+    return this.usuarios;
+}
+this.usuarioActivo=function(nick){
+    if(this.usuarios[nick]){
+        return true;
+    }
+    return false;
+}
+this.eliminarUsuario=function(nick){
+    if(this.usuarios[nick]){
+        delete this.usuarios[nick];
+    }
+}
+this.numeroUsuarios=function(){
+    return Object.keys(this.usuarios).length;
+}
+}
+function Usuario(nick){
+ this.nick=nick;
+}
+
