71c14f4d369cf6c0ccd2a996e3aaebb2d83b25b8
javier <Javier.Yago@alu.uclm.es>
2025-11-11 16:47:48 +0100

    Sprint2 arreglos finales

--- servidor/email.js
+++ servidor/email.js
@@ -36,6 +36,6 @@ module.exports.enviarEmail=async function(direccion, key, men) {
         to: direccion,
         subject: men,
         text: 'Pulsa aquí para confirmar cuenta',
-        html: '<p>Bienvenido a Sistema</p><p><a href="'+uri+'confirmarUsuario/'+direccion+'/'+key+'">Pulsa aquí para confirmar cuenta</a></p>'
+        html: '<p>Bienvenido a Sistema</p><p><a href="'+confirmUrl+'">Pulsa aquí para confirmar cuenta</a></p>'
     });
 }
